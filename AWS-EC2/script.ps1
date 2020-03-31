44865b8ec6bd825ec54ff7fa2a885bff5ca0a069
horeca2 <56235433+horeca2@users.noreply.github.com>
2020-04-01 01:49:13 +0300

    Add files via upload

---
+++ AWS-EC2/script.ps1
@@ -0,0 +1,22 @@
+﻿# Виртуальная машина Bizagi
+
+aws configure set aws_access_key_id KEY_ID
+aws configure set aws_secret_access_key ACCESS_KEY
+aws configure set region eu-north-1
+aws configure set output json
+$instance_id = "i-00000000000000"
+$user="Administrator"
+$password = 'Password';
+
+aws ec2 start-instances --instance-ids $instance_id
+
+do {
+    Write-Host "Получение IP адреса..."
+    Start-Sleep -Seconds 5
+    $ip = aws ec2 describe-instances --query Reservations[].Instances[].PublicIpAddress --instance-ids $instance_id --output text;
+
+} while (-Not ($ip -match "^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$"))
+
+cmdkey /generic:TERMSRV/$ip /user:$user /pass:$password
+mstsc /v:$ip
+
---
+++ Yandex-VM/script.ps1
@@ -0,0 +1,31 @@
+﻿yc config set token AgAAAAA4656546456546546456456456456
+yc config set cloud-id b00000000000000000000
+yc config set folder-id b0000000000000000000
+yc config set compute-default-zone ru-central1-a
+
+
+$instance_id = "0000000000000000"
+$user="Administrator"
+$password = 'password';
+
+
+yc compute instance start $instance_id
+
+do {
+    Write-Host "Получение IP адреса..."
+    Start-Sleep -Seconds 5
+    $answer = yc compute instance get --full $instance_id --format json
+    $answer
+    $data = $answer | ConvertFrom-Json
+    $ip = $data.network_interfaces.primary_v4_address.one_to_one_nat.address
+
+} while (-Not ($ip -match "^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$"))
+
+cmdkey /generic:TERMSRV/$ip /user:$user /pass:$password
+mstsc /v:$ip
+
+
+
+
+
+
